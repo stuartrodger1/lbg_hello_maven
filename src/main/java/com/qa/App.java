3d23f044249fec1037462d6115e25ae5abe650f0
stuartrodger <srodger10@hotmail.com>
2026-04-22 09:59:48 +0100

    version update with hello jordan added

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Stuart"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Jordan"));
         System.out.println(sayGoodbye());
     }
 
